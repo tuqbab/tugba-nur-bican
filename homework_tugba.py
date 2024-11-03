1cf8dcaeb85c849ebdfdcb3a639e0c07b3e983dc
tuqbab <114606988+tuqbab@users.noreply.github.com>
2024-11-03 18:33:22 +0300

    Initial Commit

---
+++ homework_tugba.py
@@ -0,0 +1,7 @@
+my_name = 'Tugba Nur'
+my_surname = 'Bican'
+my_age = '21'
+ID_number = '12345678901'
+where_i_live = 'Ankara'
+my_nationality = 'Turkish'
+print(f'My name is {my_name} {my_surname} I am {str(my_age)} years old, I live in {where_i_live} and I am {my_nationality}')
